4eaf3c38dd1010c22b0f68919424746cee69cdda
Robson Teixeira <joaorobjr@gmail.com>
2018-07-06 16:34:36 -0300

    Tranning diretives ngFor and ngModel

--- AngularJWTSpringRest/Angular/first-angular-cli/src/app/app.component.ts
+++ AngularJWTSpringRest/Angular/first-angular-cli/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First Angular Cli';
+  title = 'Tasks';
+  tasks = [];
+  task = "";
+
+  add():void{
+    this.tasks.push(this.task);
+  }
 }
